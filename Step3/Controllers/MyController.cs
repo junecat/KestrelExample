21d582a632cef44a569db1cb9b49d4bff8349ef9
Konstantin Davydov <junecat@gmail.com>
2021-02-19 07:19:19 +0300

    Ну и второй и третий шаги

---
+++ Step3/Controllers/MyController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KestrelExample.Controllers {
+    [ApiController]
+    [Route("[controller]")]
+    public class MyController : ControllerBase {
+        static readonly string[] WeekDays = new[] { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday", };
+
+        private readonly ILogger<MyController> _logger;
+
+        public MyController(ILogger<MyController> logger) {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get() {
+            var rng = new Random();
+            return Enumerable.Range(1, 7).Select(index => new string( WeekDays[rng.Next(0, 6)]))
+            .ToArray();
+        }
+
+    }
+}
---
+++ Step3/Program.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.IO;
+using System.Net;
+using Microsoft.Extensions.Configuration;
+
+namespace ServerApp {
+    public class Program {
+        public static void Main(string[] args) {
+
+            var host = new WebHostBuilder().UseKestrel(GetKso()).UseContentRoot(Directory.GetCurrentDirectory()).UseStartup<Startup>().UseWebRoot("static").Build();
+            host.Run();
+
+        }
+
+        static Action<KestrelServerOptions> GetKso() {
+            Action<KestrelServerOptions> ret = options =>
+            {
+                options.Limits.MaxConcurrentConnections = 100;
+                options.Listen(IPAddress.Any, 5000);
+            };
+            return ret;
+        }
+
+
+    }
+
+    public class Startup {
+
+        public Startup(IConfiguration configuration) {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services) {
+ 
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+            if (env.IsDevelopment()) {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints => {
+                endpoints.MapControllers();
+            });
+
+            app.Run(async (context) => {
+                await context.Response.WriteAsync("Hello, world");
+            });
+        }
+
+    }
+
+}
+
